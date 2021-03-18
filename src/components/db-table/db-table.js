3a52af6fadf905a4bfea0dcaf7e95d7517eaed3e
Cinthia Sánchez <ceenti@gmail.com>
2021-03-17 23:22:21 -0600

    Kid photo display and dashboard

--- src/components/db-table/db-table.js
+++ src/components/db-table/db-table.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Table } from 'reactstrap';
+import React, { useState, useEffect } from 'react'
+import { Table } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Video from '../../images/Video-icon-r.svg'
 import Book from '../../images/book-solid.svg'
@@ -7,19 +7,33 @@ import PDF from '../../images/Pdf-icon-a.svg'
 import { faQuestion, faArrowRight, faMinus  } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import SearchBar from '../searchbar/search-bar'
+import api from '../../lib/api'
 
 const DBTable = (props) => {
 
   const [filteredActivities, setFilteredActivities] = useState(null)
+  const [allActivities, setAllActivities] = useState([])
+
+  useEffect(() => {
+      setAllActivities(props.currentUser)
+  }, []);
 
   const filterHandler = (event) => {
-    let value = event.target.value.toLowerCase()
-    let filteredArrayMaterias = props.currentUser.filter(actividad => {
+    const value = event.target.value.toLowerCase()
+    const filteredArrayMaterias = allActivities.filter(actividad => {
       if (!actividad.resource) { return }
       return actividad.resource.title.toLowerCase().includes(value)
     })
 
     setFilteredActivities(filteredArrayMaterias)
+  }
+
+  const deleteHandler = (event) => {
+    const activity_id = event.target.dataset.id
+    if(window.confirm('¿Estás seguro de eliminar la actividad de tu historial?')) {
+      api.deleteResourceFromUserHandler(localStorage.getItem("token"), activity_id)
+      setAllActivities(allActivities.filter(actividad => actividad._id !== activity_id))
+    }
   }
 
   let cont = 1
@@ -31,10 +45,9 @@ const DBTable = (props) => {
       <Table borderless id="db-table" className="rounded">
         <thead>
           <tr className="bg-dark text-white">
-            <th>&nbsp;</th>
+            <th>Tipo</th>
             <th>Título</th>
             <th>Materia</th>
-            <th className="d-none d-lg-block">&nbsp;</th>
             <th>&nbsp;</th>
           </tr>
         </thead>
@@ -49,29 +62,24 @@ const DBTable = (props) => {
                   <tr className={cont % 2 == 0 ? "active" : "no-active"}>
                     <th scope="row">
                       <Link to={`/actividades/${_id}`} style={{ textDecoration: 'none' }} className="activity-detail-Link">
-                        <button className="plus-icon" type="button">
-                          <FontAwesomeIcon icon={faArrowRight} size="1x" color="#FE8D03"></FontAwesomeIcon>
-                        </button>
+                        {
+                          content_type === "Lectura" ? <img src={Book} alt="Book icon" width="35" />
+                            : content_type === "Video musical" || content_type === "Video" ? <img src={Video} alt="Video icon" width="35" />
+                              : content_type === "PDF" ? <img src={PDF} alt="PDF icon" width="35" />
+                                : <FontAwesomeIcon icon={faQuestion} size="1x"></FontAwesomeIcon>
+                        }
                       </Link>
                     </th>
                     <td>{title}</td>
                     <td>{category}</td>
-                    <td className="d-none d-lg-block">
-                      {
-                        content_type === "Lectura" ? <img src={Book} alt="Book icon" width="45" />
-                          : content_type === "Video musical" || content_type === "Video" ? <img src={Video} alt="Video icon" width="45" />
-                            : content_type === "PDF" ? <img src={PDF} alt="PDF icon" width="45" />
-                              : <FontAwesomeIcon icon={faQuestion} size="1x"></FontAwesomeIcon>
-                      }
-                    </td>
-                    <td><button className="plus-icon" type="button">
+                    <td><button className="plus-icon" type="button" onClick={deleteHandler} data-id={actividad._id}>
                       <FontAwesomeIcon icon={faMinus} size="1x" color="#FE8D03"></FontAwesomeIcon>
                     </button></td>
                   </tr>
                 )
               })
-              : props.currentUser ?
-                props.currentUser.map(item => {
+              : allActivities ?
+                allActivities.map(item => {
                   if (!item.resource) { return }
                   const { title, category, content_type, _id } = item.resource
                   cont = cont + 1
@@ -79,23 +87,18 @@ const DBTable = (props) => {
                     <tr className={cont % 2 == 0 ? "active" : "no-active"}>
                       <th scope="row">
                         <Link to={`/actividades/${_id}`} style={{ textDecoration: 'none' }} className="activity-detail-Link">
-                          <button className="plus-icon" type="button">
-                            <FontAwesomeIcon icon={faArrowRight} size="1x" color="#FE8D03"></FontAwesomeIcon>
-                          </button>
+                          {
+                            content_type === "Lectura" ? <img src={Book} alt="Book icon" width="35" />
+                              : content_type === "Video musical" || content_type === "Video" ? <img src={Video} alt="Video icon" width="35" />
+                                : content_type === "PDF" ? <img src={PDF} alt="PDF icon" width="35" />
+                                  : <FontAwesomeIcon icon={faQuestion} size="1x"></FontAwesomeIcon>
+                          }
                         </Link>
                       </th>
                       <td>{title}</td>
                       <td>{category}</td>
-                      <td className="d-none d-lg-block">
-                        {
-                          content_type === "Lectura" ? <img src={Book} alt="Book icon" width="45" />
-                            : content_type === "Video musical" || content_type === "Video" ? <img src={Video} alt="Video icon" width="45" />
-                              : content_type === "PDF" ? <img src={PDF} alt="PDF icon" width="45" />
-                                : <FontAwesomeIcon icon={faQuestion} size="1x"></FontAwesomeIcon>
-                        }
-                      </td>
-                      <td><button className="plus-icon" type="button">
-                        <FontAwesomeIcon icon={faMinus} size="1x" color="#FE8D03"></FontAwesomeIcon>
+                      <td><button className="plus-icon" type="button" onClick={deleteHandler} data-id={item._id}>
+                        <FontAwesomeIcon icon={faMinus} size="1x" color="#FE8D03" />
                       </button></td>
                     </tr>
                   )
